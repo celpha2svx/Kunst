f1788037e4732680e7bc6667303a99d697642435
Afeez Ademuyiwa <ademuyiwaafeez077@gmail.com>
2026-07-13 08:30:20 +0100

    fix(android): use FlutterFragmentActivity for MainActivity

--- android/app/src/main/kotlin/com/kunst/launcher/MainActivity.kt
+++ android/app/src/main/kotlin/com/kunst/launcher/MainActivity.kt
@@ -13,11 +13,11 @@ import android.os.Build
 import android.provider.Settings
 import android.provider.CalendarContract
 import androidx.core.app.ActivityCompat
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 	private var pendingCalendarPermissionResult: MethodChannel.Result? = null
 	private var pendingCalendarEventResult: MethodChannel.Result? = null
 
