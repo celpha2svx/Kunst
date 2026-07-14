16ad7d297112ba159077fafc61bf4cfbd5093599
Afeez Ademuyiwa <ademuyiwaafeez077@gmail.com>
2026-07-14 07:51:12 +0100

    Add imports for AlarmManager and PendingIntent

--- android/app/src/main/kotlin/com/kunst/launcher/BootReceiver.kt
+++ android/app/src/main/kotlin/com/kunst/launcher/BootReceiver.kt
@@ -1,8 +1,12 @@
 package com.kunst.launcher
 
+import android.app.AlarmManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+
 
 class BootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
